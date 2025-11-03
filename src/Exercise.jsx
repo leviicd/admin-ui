5994470e7bfa5887e049d86693be964be668df7c
leviicd <pahlevimuslim123@gmail.com>
2025-11-03 21:23:27 +0700

    Understanding JSX in ReactJS and ES6 (2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React,{useEffect, useState } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  	useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        <h1 className="text-3xl font bold text-center mb-6 text-blue-700">
           User Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-          name="lepi" 
-          email="levi@gmail.com"
-          street="Jl. Gergaji"
-          city="Tegal"
-        />
-          <UserCard 
-          name="cen" 
-          email="cen@gmail.com"
-          street="Jl. Pelem"
-          city="Semarang"
-        />
-          <UserCard 
-          name="mus"
-          email="mus@gmail.com"
-          street="Jl. Raya"
-          city="Bandung" 
-        />
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </div>
     </>
